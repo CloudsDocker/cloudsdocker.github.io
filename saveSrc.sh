0586197bc782669329a364040ed0ac4b8fa9fb7e
phray <phray.zhang@gmail.com>
2016-06-19 17:05:55 +0800

    daily commit

--- saveSrc.sh
+++ saveSrc.sh
@@ -1,3 +1,3 @@
-rsync -r ~/dev/hexo/myblog/*   ~/dev/hexo/cloudsdocker.github.io/
+rsync -avh ~/dev/hexo/myblog/*   ~/dev/hexo/cloudsdocker.github.io/ --delete
 rm -fr ~/dev/hexo/cloudsdocker.github.io/public
 rm -fr ~/dev/hexo/cloudsdocker.github.io/archives
